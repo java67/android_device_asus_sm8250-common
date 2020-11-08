7978396c2fce510b4210106b604a78b3d0e696d9
Jake Weinstein <jake@aospa.co>
2020-11-08 06:43:14 +0100

    sm8250-common: add init library
    - Overrides Runtime CPU to cortex-a76 for SDOT/UDOT
    - Use 6GB Dalvik Configuration

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -102,6 +102,10 @@ BOARD_ROOT_EXTRA_FOLDERS := \
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
 BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
+# Vendor init
+TARGET_INIT_VENDOR_LIB := libinit_asus_sm8250
+TARGET_RECOVERY_DEVICE_MODULES := libinit_asus_sm8250
+
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
